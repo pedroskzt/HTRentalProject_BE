f7c24dfcb509a144b040d7e811f3b4f1a2146e13
Pedro 'skzt' Nunes <pedroskzt@gmail.com>
2024-06-18 16:33:40 -0700

    Updated models to better fit their purpose.

--- api/Models/tools_model.py
+++ api/Models/tools_model.py
@@ -1,13 +1,11 @@
 from django.db import models
 
-from api.Models.tools_category_model import ToolsCategory
 from api.Models.tools_model_model import ToolsModel
 
 
 class Tools(models.Model):
-    category = models.ForeignKey(ToolsCategory, on_delete=models.SET_NULL, null=True)
     model = models.ForeignKey(ToolsModel, on_delete=models.SET_NULL, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField(null=True)
-    available = models.BooleanField(default=True)
-    image_name = models.TextField(null=True)
+    available = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.model)
