7144f1d609d17dc8acd17614b22a37ac6bfd823e
Pedro 'skzt' Nunes <pedroskzt@gmail.com>
2024-07-14 03:02:26 -0700

    Updated __str__ of user to display user full name.

--- api_auth/User/UserModel.py
+++ api_auth/User/UserModel.py
@@ -9,3 +9,7 @@ class User(AbstractUser):
     """
     address = models.CharField(max_length=255, null=True, blank=True)
     phone_number = models.CharField(max_length=255, null=True, blank=True)
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
