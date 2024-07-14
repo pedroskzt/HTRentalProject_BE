801d20c30b26b228a065e32174c24a1cc8db913c
ShEnshaei <s.enshaei@student.fdu.edu>
2024-07-13 22:48:22 -0700

    pass in requeset

--- core/ChatBotManager/ChatBotHandler.py
+++ core/ChatBotManager/ChatBotHandler.py
@@ -7,7 +7,7 @@ from core.CustomErrors.CustomErrors import CustomError
 class ChatBotHandler:
 
     @staticmethod
-    def handler_get_chat_bot_response():
+    def handler_get_chat_bot_response(request):
         try:
             # TODO: processing of input and calling google gemini api
             return Response("stub", status=status.HTTP_200_OK)
