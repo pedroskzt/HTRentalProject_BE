bccc9f3b6185bccb80847bb49bb2853e1689d662
Pedro 'skzt' Nunes <pedroskzt@gmail.com>
2024-07-14 03:50:21 -0700

    Changed tool history date to date and time.

--- api/Models/tools_history_model.py
+++ api/Models/tools_history_model.py
@@ -7,7 +7,7 @@ from api_auth.User.UserModel import User
 class ToolsHistory(models.Model):
     tool = models.ForeignKey(Tools, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    rent_start_date = models.DateField()
-    rent_end_date = models.DateField()
+    rent_start_date = models.DateTimeField()
+    rent_end_date = models.DateTimeField()
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
